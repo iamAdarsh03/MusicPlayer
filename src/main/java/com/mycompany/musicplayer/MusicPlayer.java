e786d0a1cf874cab5939324d6f4ef57901b12380
tREX <tREX@AdarshPC>
2024-10-17 03:06:15 +0530

    UI MusicPlayer

---
+++ src/main/java/com/mycompany/musicplayer/MusicPlayer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.musicplayer;
+
+/**
+ *
+ * @author tREX
+ */
+public class MusicPlayer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
